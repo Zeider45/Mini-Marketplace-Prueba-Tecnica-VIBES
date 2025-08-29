a234323a31ae3d4e570b58d1ba09220e12aae78f
Yender <yender.yr45@gmail.com>
2025-08-29 19:50:27 -0400

    feat(api): first release with JSON file data source

---
+++ api/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import productsRouter from "./products.router";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Use the cors middleware to allow access from other domains
+app.use(cors());
+
+// Use the middleware to parse the body of requests in JSON format
+app.use(express.json());
+
+app.use("/api/products", productsRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
